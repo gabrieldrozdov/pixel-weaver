7111563114f4b66aa3758f840f5e40363cb99da3
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2022-11-16 21:12:59 -0500

    new settings!

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ root.style.setProperty("--complement", `hsl(${rootHue-180},100%,50%)`);
 root.style.setProperty("--dark", `hsl(${rootHue},100%,25%)`);
 root.style.setProperty("--light", `hsl(${rootHue},25%,75%)`);
 
+let body = document.querySelector("body");
 let pageColor = document.querySelector("#pageColor");
+let toggleBackground = document.querySelector("#toggleBackground");
 pageColor.value = rootHue;
 function updateColor() {
+	if (toggleBackground.checked) {
+		body.style.backgroundColor = "white";
+	} else {
+		body.style.backgroundColor = "var(--dark)";
+	}
 	pageColor.nextElementSibling.value = rootHue;
 	rootHue = pageColor.value;
 	root.style.setProperty("--primary", `hsl(${rootHue},100%,50%)`);
@@ -35,7 +42,10 @@ let cols = [];
 // Get all menu settings
 let customSource = document.getElementById("customSource");
 let dropdownSource = document.getElementById("source");
+let toggleAngle = document.getElementById("toggleAngle");
 let sliderAngle = document.getElementById("numberAngle");
+let toggleScale = document.getElementById("toggleScale");
+let sliderScale = document.getElementById("numberScale");
 let sliderCols = document.getElementById("numberCols");
 let sliderRows = document.getElementById("numberRows");
 let sliderZoom = document.getElementById("imageZoom");
@@ -44,7 +54,7 @@ let sliderTime = document.getElementById("loopTime");
 
 // Default values for menu settings
 dropdownSource.selectedIndex = Math.floor(Math.random()*dropdownSource.options.length);
-let imageSource, numberImages, numberAngle, numberCols, numberRows, imageZoom, imageShift, looptime;
+let imageSource, numberImages, randomAngle, numberAngle, randomScale, numberScale, numberCols, numberRows, imageZoom, imageShift, looptime;
 
 // Floating buttons init
 let pause = false;
@@ -60,7 +70,10 @@ function initialize() {
 	clearInterval(gridAnim);
 	grid.innerHTML = "";
 	imageSource = dropdownSource.value;
+	randomAngle = toggleAngle.checked;
 	numberAngle = sliderAngle.value;
+	randomScale = toggleScale.checked;
+	numberScale = sliderScale.value;
 	numberCols = sliderCols.value;
 	numberRows = sliderRows.value;
 	imageZoom = sliderZoom.value;
@@ -77,6 +90,18 @@ function initialize() {
 
 	for (i=0; i<numberCols*numberRows; i++) {
 		let size = Math.random() * imageZoom + 1;
+		let angle;
+		if (randomAngle) {
+			angle = Math.random() * numberAngle;
+		} else {
+			angle = numberAngle;
+		}
+		let scale;
+		if (randomScale) {
+			scale = (Math.random() * parseInt(numberScale) + 100)/100;
+		} else {
+			scale = (parseInt(numberScale) + 100)/100;
+		}
 		let posX = Math.random() * imageShift;
 		let posY = Math.random() * imageShift;
 
@@ -89,13 +114,13 @@ function initialize() {
 			dropdownSource.options[dropdownSource.selectedIndex].text == "custom") {
 			gridTemp += `<div style='background-image:url("${imageSource}");background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;transform:rotate(${numberAngle}deg);'></div>`
 		} else if (imageSource == "grad1" || imageSource == "grad2") {
-			gridTemp += `<div style='background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;background-image:linear-gradient(${Math.floor(Math.random()*360)}deg, ${color1} 0%, ${color2} 100%);transform:rotate(${numberAngle}deg);'></div>`
+			gridTemp += `<div style='background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;background-image:linear-gradient(${Math.floor(Math.random()*360)}deg, ${color1} 0%, ${color2} 100%);transform:rotate(${angle}deg);'></div>`
 		} else if (imageSource == "grad3" || imageSource == "grad4") {
-			gridTemp += `<div style='background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;background-image:radial-gradient(circle, ${color1} 0%, ${color2} 100%);transform:rotate(${numberAngle}deg);'></div>`
+			gridTemp += `<div style='background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;background-image:radial-gradient(circle, ${color1} 0%, ${color2} 100%);transform:rotate(${angle}deg);'></div>`
 		} else if (imageSource == "random") {
-			gridTemp += `<div style='background-image:url("assets/imgs/random/img${Math.floor(Math.random()*146)}.jpg");background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;transform:rotate(${numberAngle}deg);'></div>`
+			gridTemp += `<div style='background-image:url("assets/imgs/random/img${Math.floor(Math.random()*146)}.jpg");background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;transform:rotate(${angle}deg);'></div>`
 		} else {
-			gridTemp += `<div style='background-image:url("assets/imgs/${imageSource}");background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;transform:rotate(${numberAngle}deg);'></div>`
+			gridTemp += `<div style='background-image:url("assets/imgs/${imageSource}");background-size:${size}%;background-position:${posX}% ${posY}%;transition:${loopTime}ms;transform:rotate(${angle}deg) scale(${scale});'></div>`
 		}
 	}
 	grid.innerHTML = gridTemp;
@@ -111,10 +136,23 @@ function gridAnimLoop() {
 	if (pause == false) {
 		for (var i = 0; i < gridChildren.length; i++) {
 			let size = Math.random() * imageZoom + 1;
+			let angle;
+			if (randomAngle) {
+				angle = Math.random() * numberAngle;
+			} else {
+				angle = numberAngle;
+			}
+			let scale;
+			if (randomScale) {
+				scale = (Math.random() * parseInt(numberScale) + 100)/100;
+			} else {
+				scale = (parseInt(numberScale) + 100)/100;
+			}
 			let posX = Math.random() * imageShift;
 			let posY = Math.random() * imageShift;
 			gridChildren[i].style.backgroundSize = size + "%";
 			gridChildren[i].style.backgroundPosition = `${posX}% ${posY}%`;
+			gridChildren[i].style.transform = `rotate(${angle}deg) scale(${scale})`;
 		}
 		setGrid();
 	}
@@ -144,8 +182,23 @@ function setGrid() {
 function randomize() {
 	dropdownSource.selectedIndex = Math.floor(Math.random()*dropdownSource.options.length);
 
+	if (Math.random()<.5) {
+		toggleAngle.checked = false;
+	} else {
+		toggleAngle.checked = true;
+	}
+
 	sliderAngle.value = Math.floor(Math.random()*359);
 	sliderAngle.nextElementSibling.value = sliderAngle.value;
+
+	if (Math.random()<.5) {
+		toggleScale.checked = false;
+	} else {
+		toggleScale.checked = true;
+	}
+
+	sliderScale.value = Math.round(Math.random()*250);
+	sliderScale.nextElementSibling.value = sliderScale.value;
 
 	sliderCols.value = Math.floor(Math.random()*20+1);
 	sliderCols.nextElementSibling.value = sliderCols.value;
